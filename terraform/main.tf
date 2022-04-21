72ba12b8d17e522599aab64bbe6bfdf5994a1a6d
Mark Rood <34656473+roodmp@users.noreply.github.com>
2022-04-21 14:47:42 -0700

    formatting

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,28 +3,28 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "web_server_rg" {
-  name                    = var.web_server_rg
-  location                = var.web_server_location
+  name                        = var.web_server_rg
+  location                    = var.web_server_location
 }
 
 resource "azurerm_virtual_network" "web_server_vnet" {
-  name                    = "${var.resource_prefix}-vnet"
-  address_space           = [var.web_server_address_space]
-  location                = var.web_server_location
-  resource_group_name     = azurerm_resource_group.web_server_rg.name
+  name                        = "${var.resource_prefix}-vnet"
+  address_space               = [var.web_server_address_space]
+  location                    = var.web_server_location
+  resource_group_name         = azurerm_resource_group.web_server_rg.name
 }
 
 resource "azurerm_subnet" "web_server_subnet" {
-  name                    = "${var.resource_prefix}-subnet"
-  resource_group_name     = azurerm_resource_group.web_server_rg.name
-  virtual_network_name    = azurerm_virtual_network.web_server_vnet.name
-  address_prefixes        = [var.web_server_address_prefix]
+  name                        = "${var.resource_prefix}-subnet"
+  resource_group_name         = azurerm_resource_group.web_server_rg.name
+  virtual_network_name        = azurerm_virtual_network.web_server_vnet.name
+  address_prefixes            = [var.web_server_address_prefix]
 }
 
 resource "azurerm_network_interface" "web_server_nic" {
-  name                    = "${var.web_server_name}-nic"
-  location                = var.web_server_location 
-  resource_group_name     = azurerm_resource_group.web_server_rg.name
+  name                        = "${var.web_server_name}-nic"
+  location                    = var.web_server_location
+  resource_group_name         = azurerm_resource_group.web_server_rg.name
 
   ip_configuration {
     name                          = "${var.web_server_name}-ip"
@@ -34,16 +34,16 @@ resource "azurerm_network_interface" "web_server_nic" {
 }
 
 resource "azurerm_public_ip" "web_server_public_ip" {
-  name                    = "${var.resource_prefix}-public-ip"
-  location                = var.web_server_location
-  resource_group_name     = azurerm_resource_group.web_server_rg.name
-  allocation_method       = var.environment == "production" ? "Static" : "Dynamic"
+  name                        = "${var.resource_prefix}-public-ip"
+  location                    = var.web_server_location
+  resource_group_name         = azurerm_resource_group.web_server_rg.name
+  allocation_method           = var.environment == "production" ? "Static" : "Dynamic"
 }
 
 resource "azurerm_network_security_group" "web_server_nsg" {
-  name                    = "${var.resource_prefix}-nsg"
-  location                = var.web_server_location
-  resource_group_name     = azurerm_resource_group.web_server_rg.name
+  name                        = "${var.resource_prefix}-nsg"
+  location                    = var.web_server_location
+  resource_group_name         = azurerm_resource_group.web_server_rg.name
 }
 
 resource "azurerm_network_security_rule" "web_server_nsg_rule_rdp" {
@@ -54,12 +54,12 @@ resource "azurerm_network_security_rule" "web_server_nsg_rule_rdp" {
   source_port_range           = "3389"
   destination_port_range      = "3389"
   source_address_prefix       = "*"
-  destination_address_prefix  = "*" 
+  destination_address_prefix  = "*"
   direction                   = "Inbound"
   priority                    = 100
   access                      = "Allow"
 }
-  
+
 resource "azurerm_network_interface_security_group_association" "web_server_nsg_association" {
   network_interface_id        = azurerm_network_interface.web_server_nic.id
   network_security_group_id   = azurerm_network_security_group.web_server_nsg.id
